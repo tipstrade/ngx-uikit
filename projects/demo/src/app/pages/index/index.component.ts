958f63b90480e18eb6b62f0f79164577273c62fb
tanevbass <63051634+tanevbass@users.noreply.github.com>
2025-06-06 14:27:17 +0100

    Add imports

--- projects/demo/src/app/pages/index/index.component.ts
+++ projects/demo/src/app/pages/index/index.component.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-index",
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: "./index.component.html",
   styleUrl: "./index.component.scss",
 })
