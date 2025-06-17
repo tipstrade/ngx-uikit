efdc7d534eda98f850f8fd2bf8c0510fa0058aea
John Bayly <3430117+oobayly@users.noreply.github.com.>
2025-06-17 13:55:54 +0100

    Add tests for SVG loading

--- projects/ngx-uikit/src/lib/directives/svg.directive.spec.ts
+++ projects/ngx-uikit/src/lib/directives/svg.directive.spec.ts
@@ -3,13 +3,17 @@ import { DirectiveTestContext, testUIkitDirective } from "./_directive-test-help
 import { SvgDirective } from "./svg.directive";
 
 const BadSvgMock: UIkitNS.UIkitSvgOptions = {
-  dataSrc: "https://non-existent-domain.zzz/foo.svg",
+  src: "https://non-existent-domain.zzz/foo.svg",
+};
+
+const GoodSvgMock: UIkitNS.UIkitSvgOptions = {
+  src: "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'><circle cx='50' cy='50' r='40' fill='red' /></svg>",
 };
 
 testUIkitDirective({
   name: "SvgDirective",
   template: "<img [uikitSvg]=\"options\">", type: SvgDirective,
-  expectedOptions: [[BadSvgMock, BadSvgMock]],
+  expectedOptions: [[BadSvgMock, BadSvgMock], [GoodSvgMock, GoodSvgMock]],
   otherTests: (getContext) => {
     let context: DirectiveTestContext<SvgDirective>;
 
@@ -17,11 +21,23 @@ testUIkitDirective({
       context = getContext();
     });
 
-    xit("should emit an SVG", async () => {
+    it("should emit an SVG", async () => {
+      context.fixture.componentInstance.options = GoodSvgMock;
+      context.fixture.detectChanges();
+
+      const svg = context.directiveInstance.ref?.svg;
+      const isSvg = svg?.then(x => x instanceof SVGElement);
+
+      await expectAsync(isSvg).toBeResolvedTo(true);
+    });
+
+    it("shouldn't emit an SVG", async () => {
       context.fixture.componentInstance.options = BadSvgMock;
       context.fixture.detectChanges();
 
-      await expectAsync(context.directiveInstance.ref?.svg).toBeRejected();
+      const svg = context.directiveInstance.ref?.svg;
+
+      await expectAsync(svg).toBeResolvedTo(undefined);
     });
   },
 });
